9c7e0888de2220f0e7790c64a68864ded840ce67
Ledger West <ledger.west@gmail.com>
2020-01-24 01:58:43 -0500

    feat: schedule job to consolidate daily price parts files

--- services/global_data/record_daily_prices.py
+++ services/global_data/record_daily_prices.py
@@ -82,3 +82,38 @@ def worker(event, context):
         return failure({'status': False})
 
     return success({'status': status})
+
+
+def cleanup(event, context):
+    bucket = os.environ['PRICES_BUCKET']
+    tmp_loc = './tmp/prices.json'
+
+    today = pd.Timestamp.today()
+    year = today.year
+    month = today.month
+    day = today.day
+    today = '{}-{}-{}'.format(year, month, day)
+
+    new_s3_file = '{}/prices.json'.format(today)
+
+    s3_client = boto3.client('s3')
+    res = s3_client.list_objects_v2(Bucket=bucket, Prefix=today)
+    old_objects = {'Objects': [{'Key':obj['Key']} for obj in res['Contents']]}
+
+    all_prices = pd.DataFrame()
+    for part in res['Contents']:
+        _ = s3_client.download_file(bucket, part['Key'], tmp_loc)
+        prices_df = pd.read_json(tmp_loc, orient='records')
+        all_prices = pd.concat([all_prices, prices_df], axis=0, ignore_index=True)
+
+    try:
+        all_prices.to_json(tmp_loc, orient='records')
+        _ = s3_client.upload_file(tmp_loc, bucket, new_s3_file)
+        _ = s3_client.delete_objects(Bucket=bucket, Delete=old_objects)
+        status = True
+    except:
+        e = sys.exc_info()
+        logging.info(e)
+        status = False
+
+    return success({'status': status})
